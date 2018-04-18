1f8828ccbffd6d159a2d3dd6fb3e1ba2f182597f
Chris Church <chris@ninemoreminutes.com>
2018-04-18 15:15:38 -0400

    Bump version: 0.1.3 → 0.1.4

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Nine More Minutes, Inc.'
 author = 'Nine More Minutes, Inc.'
 
 # The short X.Y version
-version = '0.1.3'
+version = '0.1.4'
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.4'
 
 
 # -- General configuration ---------------------------------------------------
