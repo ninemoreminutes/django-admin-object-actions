b1107bdb4dec882c5ca6b9af80dc6e5312171e84
Chris Church <chris@ninemoreminutes.com>
2020-08-17 13:05:27 -0400

    Bump version: 0.1.7 → 0.1.8

--- admin_object_actions/__init__.py
+++ admin_object_actions/__init__.py
@@ -1,6 +1,6 @@
 # Python
 from __future__ import unicode_literals
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 default_app_config = 'admin_object_actions.apps.AdminObjectActionsConfig'
--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Nine More Minutes, Inc.'
 author = 'Nine More Minutes, Inc.'
 
 # The short X.Y version
-version = '0.1.7'
+version = '0.1.8'
 # The full version, including alpha/beta/rc tags
-release = '0.1.7'
+release = '0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
